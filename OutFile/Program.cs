c09d9ae2f3a451bec71ff574c7a7f1ce8dd2fcd0
diegolins10 <diegofernandeslins@gmail.com>
2021-08-02 21:02:21 -0300

    Add project files.

---
+++ OutFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OutFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
